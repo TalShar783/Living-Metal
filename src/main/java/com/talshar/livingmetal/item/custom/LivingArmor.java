0ee6b53c2e3f5a2ba59fd32adee53d11fbd86957
TalShar783 <nacustis@gmail.com>
2022-02-11 12:55:29 -0500

    Caused armor to become useless when broken, but still equippable.

--- src/main/java/com/talshar/livingmetal/item/custom/LivingArmor.java
+++ src/main/java/com/talshar/livingmetal/item/custom/LivingArmor.java
@@ -1,5 +1,7 @@
 package com.talshar.livingmetal.item.custom;
 
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Multimap;
 import net.minecraft.Util;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.world.InteractionHand;
@@ -7,52 +9,38 @@ import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.Level;
 
+import java.util.UUID;
 import java.util.function.Consumer;
 
 public class LivingArmor extends ArmorItem {
-
     public LivingArmor(ArmorMaterial pMaterial, EquipmentSlot pSlot, Properties pProperties) {
         super(pMaterial, pSlot, pProperties);
     }
 
+    //TODO: Make it so enchantments won't work on broken armor.
+
     @Override
-    public boolean canEquip(ItemStack stack, EquipmentSlot armorType, Entity entity) {
+    public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlot slot, ItemStack stack) {
         if (isBroken(stack)) {
-            return false;
+            return ImmutableMultimap.of();
         } else {
-        return super.canEquip(stack, armorType, entity);}
+        return super.getAttributeModifiers(slot, stack);}
     }
 
     @Override
     public <T extends LivingEntity> int damageItem(ItemStack stack, int amount, T entity, Consumer<T> onBroken) {
-        if (isBroken(stack)) {
-            var player = (Player) entity;
-            if (!player.getInventory().add(stack)) {
-                player.drop(stack.copy(),false);
-                stack.shrink(1);
-                } else {
-                player.getInventory().add(stack);
-            }
-            return 0;
-
-        } else {
-            return super.damageItem(stack, amount, entity, onBroken);
-        }
-    }
-
-    @Override
-    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pHand) {
-        if (isBroken(pPlayer.getItemInHand(pHand))) {
-            return InteractionResultHolder.fail(pPlayer.getItemInHand(pHand));
-        } else {
-        return super.use(pLevel, pPlayer, pHand);}
+        return (isBroken(stack)) ? 0 : super.damageItem(stack, amount, entity, onBroken);
     }
 
     public static boolean isBroken(ItemStack stack) {
